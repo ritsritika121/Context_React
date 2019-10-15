a20d4347e386a8750db1ae4f2021a34501481337
Ritika <55971210+ritsritika121@users.noreply.github.com>
2019-10-15 17:09:49 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContex';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from '../LanguageSelector';
+
+class App extends React.Component {
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language });
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+            <LanguageSelector onLanguageChange={this.onLanguageChange} />
+                <ColorContext.Provider value="red">
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate />
+                    </LanguageContext.Provider> 
+                </ColorContext.Provider>
+            </div>
+        );
+
+    }
+}
+
+export default App;
